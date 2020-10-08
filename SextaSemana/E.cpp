f6b795e917a5f421c81e9998eee2a1a2a1c3e876
Lucas Xavier <lucasxaviert875@gmail.com>
2020-10-08 15:43:51 -0300

    pronto

--- SextaSemana/E.cpp
+++ SextaSemana/E.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int qtdAvos(vector<int>&vetor){
+    int qtd=1,tmp=0,tmp1=0;
+    for(int i=0;i<vetor.size();i++){
+        tmp++;
+        if(tmp>=vetor[i]){
+            tmp1=tmp;
+        }
+    }
+    return tmp1;
+}
+
 int main(){
-    int t;
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    vector<int>avos;
+    int t,n,a;
+    cin>>t;
+    while(t>0){
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cin>>a;
+            avos.push_back(a);
+        }
+        sort(avos.begin(),avos.end());
+        cout<<qtdAvos(avos)+1<<endl;
+        avos.clear();
+        t--;
+    }
 }
