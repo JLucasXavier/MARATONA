847c9c0ba93f34583deaa2cf9ba433ff34cf609d
Lucas Xavier <lucasxaviert875@gmail.com>
2020-10-22 00:52:41 -0300

    foi

--- OitavaSemana/B.cpp
+++ OitavaSemana/B.cpp
@@ -13,5 +13,13 @@ int main(){
     }
     if(2*v[2]<v[3] || 2*v[3]<v[2] || v[3]>=v[1]){
         cout<<"-1"<<endl;
+    }else{
+        cout<<2*v[0]<<endl;
+        cout<<2*v[1]<<endl;
+        if(v[2]>v[3]){
+            cout<<v[2]<<endl;
+        }else{
+            cout<<v[3]<<endl;
+        }
     }
 }
