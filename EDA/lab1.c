7c1a677ee9cb63e4f640ca909738948a06fbd25f
Lucas Xavier <lucasxaviert875@gmail.com>
2021-03-31 15:17:42 -0300

    subindo

--- EDA/lab1.c
+++ EDA/lab1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void imprime(char* s){
     if(s[0] != '\0'){
@@ -21,13 +22,30 @@ struct aluno{
     char telefone[21];
 };
 
+void inicializa(int n, Aluno** tab){
+    int i;
+    for(i=0;i<n;i++){
+        tab[i]=NULL;
+    }
+}
+
+void preenche(int n, Aluno** tab, int i,int matricula, char nome[],char endereco[],char telefone[]){
+    if(i<0 || i>=n){
+        printf("Indice fora do limite\n");
+        exit(1);
+    }
+    if(tab[i]==NULL){
+        tab[i] = (Aluno*)malloc(sizeof(Aluno));
+        tab[i]->matricula = matricula;
+        strcpy()
+    }
+}
+
 typedef struct aluno Aluno;
 //teste
 int main(){
-    //Aluno* tabela[10];
-    Aluno** tabela = (Aluno**)malloc(sizeof(Aluno*)*10);
-    //tabela[0] = (Aluno*)malloc(sizeof(Aluno));
-    tabela[0]->matricula=1921287;
+    Aluno* tabela[10];
+    //Aluno** tabela = (Aluno**)malloc(sizeof(Aluno*)*10);
     printf("A matricula  %d",tabela[0]->matricula);
     
 }
